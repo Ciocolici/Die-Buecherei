1a846759d0bf42c370b8ed2ed501b28694d458a0
Ciocolici <68241693+Ciocolici@users.noreply.github.com>
2023-09-05 17:16:10 +0200

    started the project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace Die_Buecherei
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Die Bücherei
+
+            //Schreiben Sie eine Anwendung, welche den Verleih von verschiedenen Medien(Bücher, Software, Videos) aus einer Bücherei regelt. Die maximale Verleihdauer sind 30 Tage.
+
+            //Im Folgenden wird die Aufgabe in einzelne Teilaufgaben zerlegt:
+
+            //            1.Erstellen Sie eine Basisklasse für alle ausleihbaren Medien.Dort sollen auch 3 Methoden implementiert werden: eine Methode, welche überprüft, ob ein Medium verleihbar ist(und, falls verliehen, ob die Rückgabefrist überschritten ist), eine Methode die das Ausleihen ermöglicht und eine Methode mit der die Rücknahme geregelt wird.Vergeben sie notwendige Attribute.
+            //2.Erstellen der abgeleiteten Klassen für Bücher, Software und Videos mit den notwendigen Attributen.
+            //3.Erstellen sie eine Basisklasse Person. Vergeben sie notwendige Attribute und eventuell Methoden.
+            //4.Leiten Sie davon eine Klasse Kunde ab. Vergeben sie notwendige Attribute und eventuell Methoden.
+            //5.Erstellen Sie eine Klasse Verzeichnis, in der Sie Collections definieren, die ihre Kunden und ihre verschiedenen Medien enthalten werden.
+            //6.Erstellen Sie(in der Main Methode) verschiedene Medien(vorhanden, verliehen, überzogen) und Kunden, tragen Sie diese in der Klasse Verzeichnis ein.
+            //7.Testen Sie mehrere Verleihe, Rückgaben und zeigen Sie überschrittene Rückgabefristen auf.
+
+        }
+    }
+}
